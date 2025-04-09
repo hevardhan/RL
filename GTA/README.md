7ec054a287999c3eda78827ed40c0255860d4bdb
hevardhan <paatukkorupulavan@gmail.com>
2025-04-09 19:46:50 +0530

    organizing shit

---
+++ GTA/README.md
@@ -0,0 +1,16 @@
+# pygta5
+Explorations of Using Python to play Grand Theft Auto 5.
+
+Pull requests welcome.
+
+As time goes on, I will edit this with more useful information. 
+
+The plan is to separate out the tutorial videos and "most up-to-date version." For now, I'm just exploring our possibilities of what we can actually do with Python and GTA V.
+
+The files with "part" in them are from specific tutorials. 
+
+The actual main files here are:
+
+main.py - runs the program
+directkeys.py - handles key inputs
+draw_lanes.py - finds the two "most likely" lanes out of all of the lines.
--- GTA/main.py
+++ GTA/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import time
-import pyautogui
 from directkeys import PressKey,ReleaseKey, W, A, S, D
 from draw_lanes import draw_lanes
 from grabscreen import grab_screen
@@ -88,24 +87,27 @@ for i in list(range(4))[::-1]:
     time.sleep(1)
 
 
-last_time = time.time()
-while True:
-    screen = grab_screen(region=(0,40,800,640))
-    print('Frame took {} seconds'.format(time.time()-last_time))
+    
+def main():
     last_time = time.time()
-    new_screen,original_image, m1, m2 = process_img(screen)
-    #cv2.imshow('window', new_screen)
-    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-    
-    if m1 < 0 and m2 < 0:
-        right()
-    elif m1 > 0  and m2 > 0:
-        left()
-    else:
-        straight()
-    
-    #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
 
+    while True:
+        screen = grab_screen(region=(0,40,800,640))
+        print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen,original_image, m1, m2 = process_img(screen)
+        #cv2.imshow('window', new_screen)
+        cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+
+        if m1 < 0 and m2 < 0:
+            right()
+        elif m1 > 0  and m2 > 0:
+            left()
+        else:
+            straight()
+
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+main()
